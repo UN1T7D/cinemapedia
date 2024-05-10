565db8461104869815466de810ddd514d8a6a657
UN1T7D <ahorasoysegundo@gmail.com>
2024-05-10 00:02:26 -0600

    update validate is empty query

--- lib/infraestructure/datasources/moviedb_datasource.dart
+++ lib/infraestructure/datasources/moviedb_datasource.dart
@@ -65,6 +65,7 @@ class MoviedbDatasource extends MoviesDatasource {
 
   @override
   Future<List<Movie>> searchMovie(String query) async {
+    if(query.isEmpty) return [];
     final response =
         await dio.get('/search/movie', queryParameters: {'query': query});
     return _jsonToMovie(response.data);
