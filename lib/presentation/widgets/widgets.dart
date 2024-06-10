4ff8dfe6a481d7275c7bf114b5e78a304e4e8073
UN1T7D <ahorasoysegundo@gmail.com>
2024-06-09 23:03:01 -0600

    add new widgets

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -4,3 +4,5 @@ export 'shared/full_screen_loader.dart';
 export 'movies/movies_slideshow.dart';
 export 'movies/movie_horizontal_listview.dart';
 export 'shared/custom_gradient.dart';
+export 'movies/movie_poster_link.dart';
+export 'movies/movie_masonry.dart';
